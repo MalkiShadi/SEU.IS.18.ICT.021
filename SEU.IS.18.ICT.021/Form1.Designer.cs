20e4a41d064864247616997c61546eb44b91770f
malki <malkiishadi19@gmail.com>
2024-12-03 22:30:47 +0530

    Add project files.

---
+++ SEU.IS.18.ICT.021/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SEU.IS._18.ICT._021
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bookcombo = new System.Windows.Forms.ComboBox();
+            this.Quantity = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bookcombo
+            // 
+            this.bookcombo.FormattingEnabled = true;
+            this.bookcombo.Items.AddRange(new object[] {
+            "The Lord of the Rings -19.99",
+            "Harry Potter and the Sorcerer\'s Stone -12.99",
+            "Pride and Prejudice-8.99",
+            "To Kill a Mockingbird-11.99",
+            "The Great Gatsby- 9.99",
+            "The Catcher in the Rye-10.99",
+            "1984-7.99",
+            "The Hitchhiker\'s Guide to the Galaxy-14.99",
+            "The Alchemist-12.99",
+            "The Silent Patient-16.99"});
+            this.bookcombo.Location = new System.Drawing.Point(243, 52);
+            this.bookcombo.Name = "bookcombo";
+            this.bookcombo.Size = new System.Drawing.Size(393, 21);
+            this.bookcombo.TabIndex = 0;
+            // 
+            // Quantity
+            // 
+            this.Quantity.Location = new System.Drawing.Point(243, 113);
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Size = new System.Drawing.Size(116, 20);
+            this.Quantity.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BOOK NAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "QUANTITY";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "MEMBERSHIP";
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(571, 231);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(328, 109);
+            this.output.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "club",
+            "non-club"});
+            this.comboBox1.Location = new System.Drawing.Point(243, 170);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(684, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Bill";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 377);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Quantity);
+            this.Controls.Add(this.bookcombo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox bookcombo;
+        private System.Windows.Forms.TextBox Quantity;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SEU.IS.18.ICT.021/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SEU.IS._18.ICT._021
+{
+    public partial class Form1 : Form
+    {
+        private bool isClubMember;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+           
+                var bookPrices = new Dictionary<string, decimal>
+    {
+        { "The Lord of the Rings", 19.99m },
+        { "Harry Potter and the Sorcerer's Stone", 12.99m },
+        { "Pride and Prejudice", 8.99m },
+        { "To Kill a Mockingbird", 11.99m },
+        { "The Great Gatsby", 9.99m },
+        { "The Catcher in the Rye", 10.99m },
+        { "1984", 7.99m },
+        { "The Hitchhiker's Guide to the Galaxy", 14.99m },
+        { "The Alchemist", 12.99m },
+        { "The Silent Patient", 16.99m },
+    };
+
+                // Get user input
+                string selectedBook = bookcombo.SelectedItem.ToString().Split('-')[0].Trim();
+                int quantity = int.Parse(Quantity.Text);
+                string membership = comboBox1.SelectedItem.ToString();
+                bool isClubMember;
+
+                // Calculate total price before discount
+                decimal pricePerItem = bookPrices[selectedBook];
+                decimal totalPrice = pricePerItem * quantity;
+
+                // Check for available quantity
+                int availableQuantity = 25;
+                if (quantity > availableQuantity)
+                {
+                    MessageBox.Show("Error: Entered quantity exceeds available stock!");
+                    return;
+                }
+
+             
+                decimal discount = 0;
+          
+                List<decimal> applicableDiscounts = new List<decimal>();
+                if(membership == "club")
+                {
+                        isClubMember = true;
+                }
+                else
+                {
+                        isClubMember = false;
+                }
+               
+
+
+                // Bulk Purchase Discount
+                if (!isClubMember && totalPrice >= 50)
+                {
+                    applicableDiscounts.Add(totalPrice * 0.10m);
+                }
+
+                // Club Member Discount
+                if (isClubMember)
+                {
+                    applicableDiscounts.Add(totalPrice * 0.12m);
+                }
+
+                // Series Bundle Discount
+                if (selectedBook == "Harry Potter and the Sorcerer's Stone")
+                {
+                    applicableDiscounts.Add(totalPrice * 0.15m);
+                }
+
+                // Sci-Fi/Fantasy Discount
+                if (selectedBook == "The Hitchhiker's Guide to the Galaxy")
+                {
+                    applicableDiscounts.Add(totalPrice * 0.15m);
+                }
+
+                // Self-Help Discount
+                if (selectedBook == "The Alchemist")
+                {
+                    applicableDiscounts.Add(totalPrice * 0.15m);
+                }
+
+                // Apply the top two discounts, if applicable
+                if (!isClubMember && totalPrice >= 50)
+                {
+                    // If Bulk Purchase is considered, ignore other discounts
+                    discount = applicableDiscounts.FirstOrDefault(); // Bulk Purchase is first in the list
+                }
+                else
+                {
+                    // Sort and take the top two discounts
+                    applicableDiscounts.Sort((a, b) => b.CompareTo(a)); // Sort descending
+                    discount = applicableDiscounts.Take(2).Sum();
+                }
+
+
+            // Calculate final price
+            decimal finalPrice = totalPrice - discount;
+
+            // bill
+            output.Text = $"Book: {selectedBook}{Environment.NewLine}" +
+                          $"Quantity: {quantity}{Environment.NewLine}" +
+                          $"Price/Item: ${pricePerItem}{Environment.NewLine}" +
+                          $"Total Price (Before Discount): ${totalPrice}{Environment.NewLine}" +
+                          $"Discount: ${discount}{Environment.NewLine}" +
+                          $"Total Price (After Discount): ${finalPrice}";
+        }
+        }
+    }
+
+
+
